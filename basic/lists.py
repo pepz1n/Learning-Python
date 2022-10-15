8636da4b4a38c0f67e98a13500605dbcb1f44d69
pepz1n <bernardo.zanetti@unochapeco.edu.br>
2022-10-14 21:39:19 -0300

    forget

---
+++ basic/lists.py
@@ -0,0 +1,7 @@
+contagem = [1, 2, 3, 4, 5]
+stocks = ["AAPL", "GOOG", "TSLA"]
+aleatorio = ["Pirata", ["Uma lista dentro da outra", "Socorro"]]
+
+#laço na lista
+for cont in contagem:
+  print(cont)
