2ae631fe8ea628e8a0f3a8782d653741993777ec
pepz1n <bernardo.zanetti@unochapeco.edu.br>
2022-11-25 22:22:30 -0300

    feat oi

--- oi.py
+++ oi.py
@@ -1,2 +1,13 @@
-a = "1:46:493"
-print(a.split(':'))
+import pyautogui
+from pynput.keyboard import Key, Listener 
+
+def show(key): 
+
+  if key == Key.delete: 
+    pyautogui.click(150, 300)
+    pyautogui.click(150, 300)
+  else:
+    print("Aperte DELETE Para Pickar")
+    
+with Listener(on_press = show) as listener: 
+  listener.join() 
